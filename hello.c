0205c114c18c69acf14b0b3411b91baaa9f69da3
sahsn09 <shasan9@unl.edu>
2020-01-15 22:15:35 -0600

    Update hello.c
    Adding semicolon

--- hello.c
+++ hello.c
@@ -5,8 +5,8 @@ Date: 2020/01/14
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello Sara Hasan!\n");
-	printf("English Major\n")
+	printf("Sara Hasan!\n");
+	printf("English Major\n");
 	return 0;
 }
 
